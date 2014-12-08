e0480cb95ba6af70ba0afda461c67f8c57d1f8a4
Slack06 <slack.06@mail.ru>
2014-12-08 13:12:54 +0100

    bump version number to 1.2.1

--- whatcd_yadg.user.js
+++ whatcd_yadg.user.js
@@ -2,7 +2,7 @@
 // @id             what-yadg
 // @name           what.cd - YADG
 // @description    This script provides integration with online description generator YADG (http://yadg.cc)
-// @version        1.2.0
+// @version        1.2.1
 // @namespace      yadg
 // @grant          GM_xmlhttpRequest
 // @require        https://yadg.cc/static/js/jsandbox.min.js
